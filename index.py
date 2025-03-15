eee0874979011101ffac66664e7e394d40386292
CharlesKJunior <mutebwacharles@gmail.com>
2025-03-15 22:36:28 +0300

    my first commit

---
+++ index.py
@@ -0,0 +1,25 @@
+# Basic Calculator Program
+#for mutebwa charles kasongo
+# Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+# Perform the calculation
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: You can't divide by zero Kasongo. Enter a non-zero number to try again.")
+else:
+    print("Invalid response. Please enter +, -, *, or /.")
